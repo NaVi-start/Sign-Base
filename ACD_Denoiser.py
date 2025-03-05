c243fe10aa21337e530e730fcbae8f9050a400ad
HJY <137368487+NaVi-start@users.noreply.github.com>
2025-03-05 18:14:22 +0800

    Create ACD_Denoiser.py

---
+++ ACD_Denoiser.py
@@ -0,0 +1,106 @@
+# coding: utf-8
+import torch.nn as nn
+import torch
+import math
+from torch import Tensor
+
+from helpers import freeze_params, subsequent_mask
+from transformer_layers import PositionalEncoding, TransformerDecoderLayer
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
+
+class ACD_Denoiser(nn.Module):
+
+    def __init__(self,
+                 num_layers: int = 2,
+                 num_heads: int = 4,
+                 hidden_size: int = 512,
+                 ff_size: int = 2048,
+                 dropout: float = 0.1,
+                 emb_dropout: float = 0.1,
+                 vocab_size: int = 1,
+                 freeze: bool = False,
+                 trg_size: int = 150,
+                 decoder_trg_trg_: bool = True,
+                 **kwargs):
+        super(ACD_Denoiser, self).__init__()
+
+        self.in_feature_size = trg_size #+ (trg_size // 3) * 4
+        self.out_feature_size = trg_size
+
+        self.pos_drop = nn.Dropout(p=emb_dropout)
+        self.trg_embed = nn.Linear(self.in_feature_size, hidden_size)
+        self.pe = PositionalEncoding(hidden_size, mask_count=True)
+        self.emb_dropout = nn.Dropout(p=emb_dropout)
+
+        self.layers = nn.ModuleList([TransformerDecoderLayer(
+                size=hidden_size, ff_size=ff_size, num_heads=num_heads,
+                dropout=dropout, decoder_trg_trg=i) for i in range(num_layers)])
+
+        self.layer_norm = nn.LayerNorm(hidden_size, eps=1e-6)
+
+        self.time_mlp = nn.Sequential(
+            SinusoidalPositionEmbeddings(hidden_size),
+            nn.Linear(hidden_size, hidden_size * 2),
+            nn.GELU(),
+            nn.Linear(hidden_size * 2, hidden_size),
+        )
+
+        # Output layer to be the size of joints vector + 1 for counter (total is trg_size)
+        self.output_layer = nn.Linear(hidden_size, trg_size, bias=False)
+
+        if freeze:
+            freeze_params(self)
+
+    def forward(self,
+                t,
+                trg_embed: Tensor = None,
+                encoder_output: Tensor = None,
+                src_mask: Tensor = None,
+                trg_mask: Tensor = None,
+                **kwargs):
+
+        assert trg_mask is not None, "trg_mask required for Transformer"
+        time_embed = self.time_mlp(t)[:, None, :].repeat(1, encoder_output.shape[1], 1)
+        condition = encoder_output + time_embed
+        condition = self.pos_drop(condition)
+
+        trg_embed = self.trg_embed(trg_embed)
+        # add position encoding to word embedding
+        x = self.pe(trg_embed)
+        # Dropout if given
+        x = self.emb_dropout(x)
+
+        padding_mask = trg_mask
+        # Create subsequent mask for decoding
+        sub_mask = subsequent_mask(
+            trg_embed.size(1)).type_as(trg_mask)
+
+        # Apply each layer to the input
+        for layer in self.layers:
+            x = layer(x=x, memory=encoder_output,
+                      src_mask=src_mask, trg_mask=sub_mask, padding_mask=padding_mask)
+
+        # Apply a layer normalisation
+        x = self.layer_norm(x)
+        # Output layer turns it back into vectors of size trg_size
+        output = self.output_layer(x)
+
+        return output
+
+    def __repr__(self):
+        return "%s(num_layers=%r, num_heads=%r)" % (
+            self.__class__.__name__, len(self.layers),
+            self.layers[0].trg_trg_att.num_heads)
